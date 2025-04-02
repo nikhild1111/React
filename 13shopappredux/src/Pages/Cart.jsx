180eb3be9aadd09ab7bf582faf2fd9d357388a26
nikhild1111 <160055706+nikhild1111@users.noreply.github.com>
2025-04-02 15:26:36 +0530

    2-4-25

--- 13shopappredux/src/Pages/Cart.jsx
+++ 13shopappredux/src/Pages/Cart.jsx
@@ -1,57 +1,64 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Cartitem from '../Components/Cartitem';
+import { Link } from "react-router-dom";
 import { NavLink } from 'react-router-dom';
-const Cart = () => {
 
+const Cart = () => {
   // here cart gives us the initial value
-  const {Cart}=useSelector((state)=>state);
+const { Cart } = useSelector((state) => state);
+console.log("Printing Cart");
+console.log(Cart);
+const [totalAmount, setTotalAmount] = useState(0);
 // we are extractting cart reducer directly so get initial state
 
-const[totalamount,settotalamount]=useState(0);
-useEffect(()=>{
+useEffect(() => {
+  setTotalAmount(Cart.reduce((acc, curr) => acc + curr.price, 0));
+// we get curent state and we manes it 
+}, [Cart]);//when changes occured in the Cart then it will be run
 
-  settotalamount(Cart.reduce((acc,curr)=>acc+curr.price,0));
-  // we get curent state and we manes it 
-},[Cart]);//when changes occured in the Cart then it will be run
+return (
+  <div >
+    {Cart.length > 0 ? (
+      <div className="max-w-[1200px] mx-auto flex flex-col md:flex-row justify-center">
+        <div className="w-[100%] h-[550px] md:w-[60%] flex flex-col p-2">
+          {Cart.map((item, index) => {
+                     // if we use{ } bracket then return statement is used if we use  () no need to return
+            return <Cartitem key={item.id} item={item} itemIndex={index} />;
+          })}
+        </div>
 
-
-  return (
-    <div>
-     {
-      Cart.length>0?(<div>
-        
-        <div>{
-        Cart.map((item,index)=>{
-          // if we use{ } bracket then return statement is used if we use  () no need to return
-          return <Cartitem key={item.id} item={item} itemindex={index}/>
-        })
-        }</div>
-        <div>
-
-          <div>Your Cart</div>
-           <div>Summry</div>
-           <p>
-            <span>Total Items:{Cart.length}</span>
-           </p>
+        <div className="w-[100%] md:w-[40%] mt-5  flex flex-col">
+          <div className="flex flex-col p-5 gap-5 my-14  h-[100%] justify-between">
+          <div className="flex flex-col gap-5 ">
+          <div className="font-semibold text-xl text-green-800 ">Your Cart</div>
+            <div className="font-semibold text-5xl text-green-700  -mt-5">Summary</div>
+            <p className="text-xl">
+              <span className="text-gray-700 font-semibold text-xl">Total Items: {Cart.length}</span>
+            </p>
           </div>
-          <div><p>
-                 Total Amount:${totalamount}
-                 </p></div>
-        <button>CheckOut NoW</button>
-        
-        </div>):(<div>
+          </div>
 
-          <h1>Cart Empty</h1>
-          <NavLink to="/">
-          <button>
+          <div className="flex flex-col">
+            <p className="text-xl font-bold"><span className="text-gray-700 font-semibold">Total Amount:</span> ${totalAmount}</p>
+            <button className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl">CheckOut Now</button>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div className="min-h-[80vh] flex flex-col items-center justify-center">
+        <h1 className="text-gray-700 font-semibold text-xl mb-2">
+          Your cart is empty!
+        </h1>
+        <Link to={"/"}>
+          <button className="uppercase bg-green-600 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-semibold hover:text-green-700 p-3 px-10 tracking-wider">
             Shop Now
           </button>
-          </NavLink>
-        </div>)
-     }
-    </div>
-  )
-}
+        </Link>
+      </div>
+    )}
+  </div>
+);
+};
 
-export default Cart
+export default Cart;
